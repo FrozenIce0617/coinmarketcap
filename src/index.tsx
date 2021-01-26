bb7b5fc0cafbe4b773ee80f92b5a6bf4df6642f3
FrozenIce0617 <prosdev087@gmail.com>
2021-01-26 23:31:31 +0000

    chore: update console log option

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const noop = () => {};
+
+if (process.env.NODE_ENV !== 'development') {
+  console.log = noop;
+  console.warn = noop;
+  console.error = noop;
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
